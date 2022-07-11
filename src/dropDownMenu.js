38239d8ce32b6cef78898f368c086255f5ccf844
budy6991 <alvmenfer@gmail.com>
2022-07-11 11:59:11 +0200

    Functionality for color and title added

--- src/dropDownMenu.js
+++ src/dropDownMenu.js
@@ -1,1 +1,15 @@
-// We will have to first create a function, that writes the content of the dropdown menu. By inputin
+function createDiv(classList, color) {
+  const body = document.querySelector('body');
+  const mainDiv = document.createElement('div');
+  mainDiv.classList.add(classList);
+  mainDiv.style.flex = 'auto';
+  mainDiv.style.backgroundColor = color;
+  mainDiv.textContent = classList;
+  body.appendChild(mainDiv);
+}
+
+function createDropDownMenu(title, color, ...subTitles) {
+  createDiv(title, color);
+}
+
+export { createDropDownMenu, createDiv };
